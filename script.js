4c0ba37d1bd1c25bce51823fcd1bafee3196e66b
Dereje <dereje2012k@gmail.com>
2025-05-11 13:42:51 +0300

    Add UI for the calculator

--- script.js
+++ script.js
@@ -14,9 +14,18 @@ function divide(num1, num2) {
   return num1 / num2;
 }
 
+function operate(operator, operandA, operandB) {
+  return operator(operandA, operandB); // if not return, the value will be undefined.
+}
+
+let num1;
+let num2;
+let operator;
+
 console.log(`
-  add: ${add(5, 9)},
-  sub: ${subtract(8, 2)},
-  mul: ${multiply(9, 6)},
-  div: ${divide(8, 4)}
+  add: ${operate(add, 5, 9)},
+  sub: ${operate(subtract, 8, 2)},
+  mul: ${operate(multiply, 9, 6)},
+  div: ${operate(divide, 8, 4)}
+
   `);
