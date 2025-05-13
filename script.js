614a2ac4ce5c5a7559f626024566986849bf41a2
Dereje <dereje2012k@gmail.com>
2025-05-13 08:19:55 +0300

    Refactor button click event listner
    implement button click handler for numbers and operators

--- script.js
+++ script.js
@@ -14,31 +14,76 @@ function divide(num1, num2) {
   return num1 / num2;
 }
 
-function operate(operator, operandA, operandB) {
+function operate(operandA, operandB, operator) {
   return operator(operandA, operandB); // if not return, the value will be undefined.
 }
 
 function populate(target) {
-  display.textContent = target.textContent;
+  display.textContent = typeof target === "string" ? target : target.join("");
 }
 
 const display = document.querySelector(".display");
 const buttons = document.querySelectorAll(".board-container");
-buttons.forEach((button) => {
-  button.addEventListener("click", (event) => {
-    const target = event.target;
-    populate(target);
-  });
-});
-
-let num1;
-let num2;
+let num1 = [];
+let num2 = [];
 let operator;
 
-console.log(`
-  add: ${operate(add, 5, 9)},
-  sub: ${operate(subtract, 8, 2)},
-  mul: ${operate(multiply, 9, 6)},
-  div: ${operate(divide, 8, 4)}
+function handleNum1(event) {
+  const numbers = "0, 1, 2, 3, 4, 5, 6, 7, 8, 9";
+  const operators = "+, -, *, /";
+  if (numbers.includes(event.target.textContent)) {
+    num1.push(event.target.textContent);
+    populate(num1);
+  } else if (operators.includes(event.target.textContent)) {
+    operator = event.target.textContent;
+    populate(operator);
+    buttons.forEach((button) => {
+      button.removeEventListener("click", handleNum1);
+    });
+    addEvLi(handleOperator);
+  } else {
+    buttons.forEach((button) => {
+      button.removeEventListener("click", handleNum1);
+    });
+  }
+}
 
-  `);
+function handleOperator(event) {
+  const operators = "+, -, *, /";
+  const numbers = "0, 1, 2, 3, 4, 5, 6, 7, 8, 9";
+  if (operators.includes(event.target.textContent)) {
+    operator = event.target.textContent;
+    populate(operator);
+  } else if (numbers.includes(event.target.textContent)) {
+    num2.push(event.target.textContent);
+    populate(num2);
+    buttons.forEach((button) => {
+      button.removeEventListener("click", handleOperator);
+    });
+    addEvLi(handleNum2);
+  } else {
+    buttons.forEach((button) => {
+      button.removeEventListener("click", handleOperator);
+    });
+  }
+}
+
+function handleNum2(event) {
+  const numbers = "0, 1, 2, 3, 4, 5, 6, 7, 8, 9";
+  if (numbers.includes(event.target.textContent)) {
+    num2.push(event.target.textContent);
+    populate(num2);
+  } else {
+    buttons.forEach((button) => {
+      button.removeEventListener("click", handleNum2);
+    });
+  }
+}
+
+function addEvLi(operand) {
+  buttons.forEach((button) => {
+    button.addEventListener("click", operand);
+  });
+}
+
+addEvLi(handleNum1);
